c1c710ceef18ca8d31d925514607282c7ca37a6b
taka-sakamoto <goodspeed855@gmail.com>
2026-03-06 14:04:55 +0900

    Initial Commit

---
+++ CatImageApp/CatImageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatImageAppApp.swift
+//  CatImageApp
+//
+//  Created by Takayuki Sakamoto on 2026/03/06.
+//
+
+import SwiftUI
+
+@main
+struct CatImageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
