13a9d9681d9ad64194f86b8570557110311dfdd0
taka-sakamoto <goodspeed855@gmail.com>
2026-03-06 14:57:56 +0900

    Add SwiftUI Cat Image API sample

---
+++ CatImageApp/Cat.swift
@@ -0,0 +1,18 @@
+//
+//  Cat.swift
+//  CatImageApp
+//
+//  Created by Takayuki Sakamoto on 2026/03/06.
+//
+
+import SwiftUI
+
+struct Cat: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Cat()
+}
---
+++ CatImageApp/CatViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CatViewModel.swift
+//  CatImageApp
+//
+//  Created by Takayuki Sakamoto on 2026/03/06.
+//
+
+import Foundation
