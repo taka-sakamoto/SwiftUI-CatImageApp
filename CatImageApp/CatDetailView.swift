875d8e022e84b92603d3d2c84617c8299df21138
taka-sakamoto <goodspeed855@gmail.com>
2026-03-07 14:40:26 +0900

    Add cat list feature

---
+++ CatImageApp/CatDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  CatDetailView.swift
+//  CatImageApp
+//
+//  Created by Takayuki Sakamoto on 2026/03/07.
+//
+
+import SwiftUI
+
+struct CatDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CatDetailView()
+}
